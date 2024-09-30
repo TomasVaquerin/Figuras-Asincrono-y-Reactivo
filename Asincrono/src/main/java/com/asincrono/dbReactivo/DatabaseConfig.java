3dc89a2e2cc810b2d67a4de3dc4c75d6d2f82b83
KevinSanchez5 <@KevinSanchez5>
2024-09-30 23:30:51 +0200

    Inicio base r2dbc

---
+++ Asincrono/src/main/java/com/asincrono/dbReactivo/DatabaseConfig.java
@@ -0,0 +1,53 @@
+package com.asincrono.dbReactivo;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
+
+public class DatabaseConfig {
+
+    public static DatabaseConfig instance;
+
+    private static ConnectionFactory connectionFactory;
+
+    public static synchronized DatabaseConfig getInstance(){
+        if(instance == null){
+            instance = new DatabaseConfig();
+        }
+        return instance;
+    }
+
+    private DatabaseConfig(){
+        Properties properties = new Properties();
+        try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("database.properties")) {
+            if (input == null) {
+                throw new IOException("No se pudo cargar el archivo de propiedades");
+            }
+            properties.load(input);
+            connectionFactory = ConnectionFactories.get(ConnectionFactoryOptions.builder()
+                    .option(DRIVER, "h2")
+                    .option(PROTOCOL, "mem")  // mem significa que es en memoria
+                    .option(DATABASE, "testdb")
+                    .option(USER, properties.getProperty("r2dbc.username", "sa"))
+                    .option(PASSWORD, properties.getProperty("r2dbc.password", "ad"))
+                    .build());
+        } catch (IOException e) {
+            throw new RuntimeException("No se pudo cargar el archivo de propiedades", e);
+        }
+    }
+
+    public static ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+
+
+}
+
---
+++ Asincrono/src/main/java/com/asincrono/dbReactivo/DatabaseInitializer.java
@@ -0,0 +1,44 @@
+package com.asincrono.dbReactivo;
+
+
+import io.r2dbc.spi.ConnectionFactory;
+import reactor.core.publisher.Mono;
+
+public class DatabaseInitializer {
+
+    private final ConnectionFactory connectionFactory;
+
+    public DatabaseInitializer() {
+        this.connectionFactory = DatabaseConfig.getInstance().getConnectionFactory();
+    }
+
+    public Mono<Void> initialize() {
+        dropTable();
+        return Mono.from(connectionFactory.create())
+                .flatMap(connection -> {
+                    return Mono.from(connection.createStatement(
+                                    "CREATE TABLE IF NOT EXISTS figuras (" +
+                                            "id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
+                                            "cod UUID UNIQUE DEFAULT RANDOM_UUID(), " +
+                                            "my_id BIGINT GENERATED ALWAYS AS IDENTITY, " +
+                                            "nombre VARCHAR(255) NOT NULL, " +
+                                            "modelo ENUM('MARVEL', 'DISNEY', 'ANIME', 'DEPORTE', 'MUSICA', 'OTROS') NOT NULL, " +
+                                            "precio DECIMAL(10,2), " +
+                                            "fecha_lanzamiento DATE, " +
+                                            "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
+                                            "updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)"
+                            ).execute())
+                            .doFinally(signal -> connection.close());
+                }).then();
+    }
+
+    public Mono<Void> dropTable() {
+        return Mono.from(connectionFactory.create())
+                .flatMap(connection -> {
+                    return Mono.from(connection.createStatement(
+                                    "DROP TABLE IF EXISTS tiempo_localidades"
+                            ).execute())
+                            .doFinally(signal -> connection.close());
+                }).then();
+    }
+}
