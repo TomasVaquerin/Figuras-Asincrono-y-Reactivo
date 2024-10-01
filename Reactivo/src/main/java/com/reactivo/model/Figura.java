b695a4a6f7127bae88b26c08a90c1a9597e2c631
KevinSanchez5 <@KevinSanchez5>
2024-10-01 22:55:03 +0200

    HotFix Figura y metodos findAll y findById en clase FiguraRepositoryImpl

--- Reactivo/src/main/java/com/reactivo/model/Figura.java
+++ Reactivo/src/main/java/com/reactivo/model/Figura.java
@@ -1,12 +1,16 @@
 package com.reactivo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Figura {
     private Long id;
     private UUID cod;
--- Reactivo/src/main/java/com/reactivo/repository/FiguraRepositoryImpl.java
+++ Reactivo/src/main/java/com/reactivo/repository/FiguraRepositoryImpl.java
@@ -1,35 +1,91 @@
 package com.reactivo.repository;
 
+import com.reactivo.dbreactivo.DatabaseConfig;
 import com.reactivo.model.Figura;
 import com.reactivo.model.Modelo;
+import io.r2dbc.spi.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 public class FiguraRepositoryImpl implements FiguraRepository {
 
+    private final ConnectionFactory connectionFactory;
 
-    @Override
-    public Flux<Figura> findAllByModelo(Modelo modelo) {
-        return null;
+    private final Logger logger = LoggerFactory.getLogger(FiguraRepositoryImpl.class);
+
+    public FiguraRepositoryImpl(ConnectionFactory connectionFactory) {
+        DatabaseConfig.getInstance();
+        this.connectionFactory = DatabaseConfig.getConnectionFactory();
     }
+
+
 
     @Override
     public Flux<Figura> findAll() {
+        String query = "SELECT * FROM figuras";
+        logger.debug("(REPO)Buscando todas las figuras en la base de datos");
+
+        return Flux.from(connectionFactory.create())
+                .flatMap(connection -> {
+                    return Flux.from(connection.createStatement(query).execute())
+                            .flatMap(result -> result.map((row, rowMetadata) -> {
+                                return new Figura(
+                                        row.get("id", Long.class),
+                                        UUID.fromString(row.get("cod", String.class)),
+                                        row.get("my_id", Long.class),
+                                        row.get("nombre", String.class),
+                                        row.get("modelo", Modelo.class),
+                                        row.get("precio", Double.class),
+                                        row.get("fecha_lanzamiento", java.time.LocalDate.class),
+                                        row.get("created_at", java.time.LocalDateTime.class),
+                                        row.get("updated_at", java.time.LocalDateTime.class)
+                                );
+                            }))
+                            .switchIfEmpty(Flux.empty())
+                            .doFinally(signal -> connection.close());
+                });
+        }
+
+    @Override
+    public Mono<Figura> findById(Long id) {
+        String query = "SELECT * FROM figuras WHERE id = $1";
+        logger.debug("(REPO)Buscando la figura con id: " + id);
+
+        return Mono.from(connectionFactory.create())
+                .flatMap(connection ->
+                        Mono.from(connection.createStatement(query)
+                            .bind("$1", id)
+                            .execute())
+                            .flatMap(result -> Mono.from(result.map((row, rowMetadata) ->
+                                    new Figura(
+                                        row.get("id", Long.class),
+                                        UUID.fromString(row.get("cod", String.class)),
+                                        row.get("my_id", Long.class),
+                                        row.get("nombre", String.class),
+                                        row.get("modelo", Modelo.class),
+                                        row.get("precio", Double.class),
+                                        row.get("fecha_lanzamiento", java.time.LocalDate.class),
+                                        row.get("created_at", java.time.LocalDateTime.class),
+                                        row.get("updated_at", java.time.LocalDateTime.class)
+                                )
+                            )))
+                            .switchIfEmpty(Mono.empty())
+                            .doFinally(signal -> connection.close())
+                );
+    }
+
+
+    @Override
+    public Mono<Figura> save(Figura object) {
         return null;
     }
 
     @Override
-    public Mono<Figura> findById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Mono<Figura> save(Figura figura) {
-        return null;
-    }
-
-    @Override
-    public Mono<Figura> update(Long aLong, Figura figura) {
+    public Mono<Figura> update(Long aLong, Figura object) {
         return null;
     }
 
@@ -37,4 +93,10 @@ public class FiguraRepositoryImpl implements FiguraRepository {
     public Mono<Void> delete(Long aLong) {
         return null;
     }
+
+    @Override
+    public Flux<Figura> findAllByModelo(Modelo modelo) {
+        return null;
+    }
 }
+
